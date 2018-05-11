154b4f4e86198008a7fb73877b089b3d59977827
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-05-11 12:35:08 +1000

    Update Counter.vhd

--- TrafficLight/Counter.vhd
+++ TrafficLight/Counter.vhd
@@ -3,27 +3,42 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity Counter is
-    Port ( reset : in  STD_LOGIC;
-           clock : in  STD_LOGIC;
-           delay_1s : out  STD_LOGIC);
+    Port ( Reset : in  STD_LOGIC; --external reset
+           Clock : in  STD_LOGIC; --clock
+			  Clear : in STD_LOGIC; --internal reset
+			  CountEn : in STD_LOGIC; --enable the counter
+           Delay_1s : out  STD_LOGIC); --wait for the delay so the lights dont change instantly
 end Counter;
 
 architecture Behavioral of Counter is
-	signal count : natural range 0 to 200;
+	constant DELAY : integer := 100; -- 1 second
+	signal Count : natural range 0 to DELAY; --counter
 begin
-	timer:
-   process (reset, clock)
+	Timer:
+   process (Reset, Clock)
    begin
-      if (reset = '1') then
-         count <= 1;
+      if (Reset = '1') then --asynch reset
+         Count <= 1;
+			Delay_1s <= '0';
       elsif rising_edge(clock) then
-			count <= count+1;
+			if Clear = '1' then --synch clear to rest flip flops
+				Count <= 0;
+				Delay_1s <= '0';
+			else
+				if CountEn = '1' then -- if count is enabled
+					if Count < DELAY then
+						Count <= Count + 1; -- count
+					else
+						Count <= 0;
+					end if;
+				else
+					Count <= 0; --else dont
+				end if;
+				if Count = DELAY then
+					Delay_1s <= '1'; --after a delay set this velue high
+				end if;
+			end if;
       end if;
    end process;
-	
-	-- output statements
-		--if clock = this then do that
-		--use for delays
-	delay_1s <= '1' when count = 1 else '0';
 
 end Behavioral;
