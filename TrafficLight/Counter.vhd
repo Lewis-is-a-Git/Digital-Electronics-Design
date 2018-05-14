8b26cc9eb41910fef8782099c5c0e60c248a3565
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-05-14 16:46:28 +1000

    Update Counter.vhd

--- TrafficLight/Counter.vhd
+++ TrafficLight/Counter.vhd
@@ -5,26 +5,22 @@ use IEEE.NUMERIC_STD.ALL;
 entity Counter is
     Port ( Reset : in  STD_LOGIC; --external reset
            Clock : in  STD_LOGIC; --clock
-			  Clear : in STD_LOGIC; --internal reset
-			  CountEn : in STD_LOGIC; --enable the counter
+			  CountEn : in STD_LOGIC; --enable the counter also clears
            Delay_1s : out  STD_LOGIC); --wait for the delay so the lights dont change instantly
 end Counter;
 
 architecture Behavioral of Counter is
-	constant DELAY : integer := 100; -- 1 second
+	constant DELAY : integer := 50; -- 1 second
 	signal Count : natural range 0 to DELAY; --counter
 begin
 	Timer:
    process (Reset, Clock)
    begin
+		
       if (Reset = '1') then --asynch reset
          Count <= 1;
 			Delay_1s <= '0';
       elsif rising_edge(clock) then
-			if Clear = '1' then --synch clear to rest flip flops
-				Count <= 0;
-				Delay_1s <= '0';
-			else
 				if CountEn = '1' then -- if count is enabled
 					if Count < DELAY then
 						Count <= Count + 1; -- count
@@ -33,11 +29,11 @@ begin
 					end if;
 				else
 					Count <= 0; --else dont
+					Delay_1s <= '0';
 				end if;
-				if Count = DELAY then
+				if Count > DELAY - 1 then
 					Delay_1s <= '1'; --after a delay set this velue high
 				end if;
-			end if;
       end if;
    end process;
 
