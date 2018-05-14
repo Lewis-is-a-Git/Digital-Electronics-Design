824a947f7b38578a85130434f6559e476c5716aa
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-05-14 16:46:03 +1000

    Update Controller.vhd

--- TrafficLight/Controller.vhd
+++ TrafficLight/Controller.vhd
@@ -10,8 +10,7 @@ entity Controller is
 			  CarEW : in STD_LOGIC; --car east west button
 			  CarNS : in STD_LOGIC; -- car north south button
 			  Delay_1s : in STD_LOGIC; --delay to slow down the system
-			  Clear : out STD_LOGIC; --internal signal to communicate between controller and counter to rest count
-			  CountEn : out STD_LOGIC; --only use the counter if it is enabled
+			  CountEn : out STD_LOGIC; --only use the counter if it is enabled also clears
 			  LightsEW : out STD_LOGIC_VECTOR(1 downto 0); --light output east west
 			  LightsNS : out STD_LOGIC_VECTOR(1 downto 0) --light output north south
 			  );
@@ -25,27 +24,37 @@ architecture Behavioral of Controller is
 	constant WALK  : std_logic_vector(1 downto 0) := "11";
 	
 	--state machine types
-	type StateType is (NSGreen, NSAmber, EWGreen, EWAmber);
+	type StateType is (NSGreen, NSWalk, NSAmber, EWGreen, EWWalk, EWAmber);
 	signal State, Nextstate : Statetype;
 	
 	--remeber if a pedestrain button is pressed
 	signal PedNSButtonPressed, PedEWButtonPressed : std_logic;
 	signal ClearPedNSButtonPressed, ClearPedEWButtonPressed : std_logic;
+	signal CarNSButtonPressed, CarEWButtonPressed : std_logic;
+	signal ClearCarNSButtonPressed, ClearCarEWButtonPressed : std_logic;
 begin
    StateProcess:
-   process (Reset, Clock, State, ClearPedNSButtonPressed, ClearPedEWButtonPressed)
+   process (Reset, Clock, State, ClearPedNSButtonPressed, ClearPedEWButtonPressed, ClearCarNSButtonPressed, ClearCarEWButtonPressed)
    begin
       if (reset = '1') then --external reset
          State <= NSGreen;
 			PedNSButtonPressed <= '0';
 			PedEWButtonPressed <= '0';
+			CarEWButtonPressed <= '0';
+			CarNSButtonPressed <= '0';
       elsif rising_edge(clock) then
 			--remeber if a button is pressed
 			if PedEW = '1' then
-				PedNSButtonPressed <= '1';
-			elsif PedNS = '1' then
 				PedEWButtonPressed <= '1';
+			elsif PedNS = '1' then
+				PedNSButtonPressed <= '1';
 			end if;
+			if CarEW = '1' then
+				CarEWButtonPressed <= '1';
+			elsif CarNS = '1' then
+				CarNSButtonPressed <= '1';
+			end if;
+			
 			--go to next state
 			State <= NextState;
       end if;
@@ -56,10 +65,17 @@ begin
 		if ClearPedEWButtonPressed = '1' then
 			PedEWButtonPressed <= '0';
 		end if;
+		--cars
+		if ClearCarNSButtonPressed = '1' then
+			CarNSButtonPressed <= '0';
+		end if;
+		if ClearCarEWButtonPressed = '1' then
+			CarEWButtonPressed <= '0';
+		end if;
    end process StateProcess;
    
 	CombinationalProcess:
-	process(State, PedNSButtonPressed, PedEWButtonPressed, CarEW, CarNS, Delay_1s)
+	process(State, PedNSButtonPressed, PedEWButtonPressed, CarEWButtonPressed, CarNSButtonPressed, Delay_1s)
 	begin
 		-- default values for outputs
 		LightsEW <= RED;
@@ -67,47 +83,83 @@ begin
 		Nextstate <= State;
 		ClearPedNSButtonPressed <= '0';
 		ClearPedEWButtonPressed <= '0';
+		ClearCarNSButtonPressed <= '0';
+		ClearCarEWButtonPressed <= '0';
 		CountEn <= '0';
-		Clear <= '0';
 		
 		--state machine
 		case State is
+			--NSGreen
 			when NSGreen =>
-				if PedNSButtonPressed = '1' then
-					LightsNS <= WALK;
-					ClearPedNSButtonPressed <= '1';
-				else
-					LightsNS <= GREEN;
-				end if;
-				if PedEWButtonPressed = '1' or CarEW = '1' then --wait for input from the other direction
-					Clear <= '1'; --reset the counter
+				LightsNS <= GREEN;
+				if PedEWButtonPressed = '1' or CarEWButtonPressed = '1' then --wait for input from the other direction
 					CountEn <= '1'; -- enable the counter
 					if Delay_1s = '1' then --wait for the delay
 						NextState <= NSAmber; --go to next state
+						ClearCarNSButtonPressed <= '1';
+						CountEn <= '0';
 					end if;
 				end if;
+			--NSwalk
+			when NSWalk =>
+				LightsNS <= WALK;
+				if PedEWButtonPressed = '1' or CarEWButtonPressed = '1' then --wait for input from the other direction
+					CountEn <= '1'; -- enable the counter
+					if Delay_1s = '1' then --wait for the delay
+						NextState <= NSAmber; --go to next state
+						ClearCarNSButtonPressed <= '1';
+						ClearPedNSButtonPressed <= '1';
+						CountEn <= '0';
+					end if;
+				end if;
+			--NSAmber
 			when NSAmber => --swap directions of lights
 				LightsNS <= AMBER;
-				NextState <= EWGreen;
-			when EWGreen =>
-				if PedEWButtonPressed = '1' then
-					LightsEW <= WALK;	
-					ClearPedEWButtonPressed <= '1';
-				else
-					LightsEW <= GREEN;
+				CountEn <= '1';
+				if Delay_1s = '1' then --wait for the delay
+					if PedEWButtonPressed = '1' then
+						NextState <= EWWalk;
+					else
+						NextState <= EWGreen;
+					end if;
+					CountEn <= '0';
 				end if;
-				if PedNSButtonPressed = '1' or CarNS = '1' then
-					Clear <= '1';
-					CountEn <= '1';
-					if Delay_1s = '1' then
-						NextState <= EWAmber;
+			--EWGreen
+			when EWGreen =>
+					LightsEW <= GREEN;
+					ClearCarEWButtonPressed <= '1';
+				if PedNSButtonPressed = '1' or CarNSButtonPressed = '1' then --wait for input from the other direction
+					CountEn <= '1'; -- enable the counter
+					if Delay_1s = '1' then --wait for the delay
+						NextState <= EWAmber; --go to next state
+						CountEn <= '0';
 					end if;
 				end if;
+			--EWWalk
+			when EWWalk =>
+				LightsEW <= WALK;
+				ClearPedEWButtonPressed <= '1';
+				ClearCarEWButtonPressed <= '1';
+
+				if PedNSButtonPressed = '1' or CarNSButtonPressed = '1' then --wait for input from the other direction
+					CountEn <= '1'; -- enable the counter
+					if Delay_1s = '1' then --wait for the delay
+						NextState <= EWAmber; --go to next state
+						CountEn <= '0';
+					end if;
+				end if;
+			--EWAMber
 			when EWAmber =>
 				LightsEW <= AMBER;
-				NextState <= NSGreen;
+				CountEn <= '1'; -- enable the counter
+				if Delay_1s = '1' then --wait for the delay
+					if PedNSButtonPressed = '1' then
+						NextState <= NSWalk;
+					else
+						NextState <= NSGreen;
+					end if;
+					CountEn <= '0';
+				end if;
 		end case;
 	end process;
-
 end Behavioral;
-
