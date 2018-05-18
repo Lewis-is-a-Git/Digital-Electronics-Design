0b0efc80c2bc2e18db27c36b059f96700aa1749e
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-05-18 20:25:22 +1000

    Update Counter.vhd

--- TrafficLight/Counter.vhd
+++ TrafficLight/Counter.vhd
@@ -5,36 +5,36 @@ use IEEE.NUMERIC_STD.ALL;
 entity Counter is
     Port ( Reset : in  STD_LOGIC; --external reset
            Clock : in  STD_LOGIC; --clock
-			  CountEn : in STD_LOGIC; --enable the counter also clears
-           Delay_1s : out  STD_LOGIC); --wait for the delay so the lights dont change instantly
+			  CountEn : in STD_LOGIC; --enable the counter, also clears
+			  --wait for the delay so the lights dont change on clock edge
+           Delay_1s : out  STD_LOGIC;
+			  Delay_2s : out  STD_LOGIC;
+           Delay_3s : out  STD_LOGIC); 			  
 end Counter;
 
 architecture Behavioral of Counter is
-	constant DELAY : integer := 100; -- 1 second
+	constant DELAY : integer := 300; -- 3 seconds
 	signal Count : natural range 0 to DELAY; --counter
 begin
 	Timer:
    process (Reset, Clock)
    begin
-		
       if (Reset = '1') then --asynch reset
-         Count <= 1;
-			Delay_1s <= '0';
+         Count <= 0;
       elsif rising_edge(clock) then
-				if CountEn = '1' then -- if count is enabled
-					if Count < DELAY then
-						Count <= Count + 1; -- count
-					else
-						Count <= 0;
-					end if;
+			if CountEn = '1' then -- if count is enabled
+				if Count < DELAY then
+					Count <= Count + 1; -- count
 				else
-					Count <= 0; --else dont
-					Delay_1s <= '0';
+					Count <= 0;
 				end if;
-				if Count > DELAY - 1 then
-					Delay_1s <= '1'; --after a delay set this velue high
-				end if;
+			else
+				Count <= 0; --else dont
+			end if;
       end if;
    end process;
-
+	
+	Delay_1s <= '1' when (Count = 100) else '0'; -- 1 second
+   Delay_2s <= '1' when (Count = 200) else '0'; -- 2 seconds
+	Delay_3s <= '1' when (Count = 300) else '0'; -- 3 seconds
 end Behavioral;
